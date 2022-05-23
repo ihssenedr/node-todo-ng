b39c2f5438fed9babb1b724ec0c565fb99b98b66
ihssene drine <ihsene.drine@asm-dev.com>
2022-05-23 20:30:17 +0200

    debloy to heroku

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import express, { Application } from "express"
 import cors from "cors"
 
 const app: Application = express()
-const PORT = 3000
+const PORT = process.env.PORT ||3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
